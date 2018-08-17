e10ae36d2494fa08f62f93038007c00fd0290adb
Mustafa Al-Bassam <mus@musalbas.com>
2018-08-17 02:56:28 +0100

    Get() working

--- smt.go
+++ smt.go
@@ -11,12 +11,15 @@ type SparseMerkleTree struct {
     depth uint
     hasher hash.Hash
     ms MapStore
+    root []byte
 }
 
 // Initialise a Sparse Merkle tree on an empty MapStore.
-func NewSparseMerkleTree(ms MapStore, defaultValue []byte, depth uint, hasher hash.Hash) []byte {
+func NewSparseMerkleTree(ms MapStore, defaultValue []byte, depth uint, hasher hash.Hash) *SparseMerkleTree {
     var currentValue, currentHash []byte
-    currentValue = defaultValue
+    hasher.Write(defaultValue)
+    currentValue = hasher.Sum(nil)
+    ms.Put(currentValue, defaultValue)
     for i := uint(0); i < depth; i++ {
         currentValue = append(currentValue, currentValue...)
         hasher.Write(currentValue)
@@ -25,5 +28,47 @@ func NewSparseMerkleTree(ms MapStore, defaultValue []byte, depth uint, hasher ha
         currentValue = make([]byte, len(currentHash))
         copy(currentValue, currentHash)
     }
-    return currentHash
+    return &SparseMerkleTree{
+        defaultValue: defaultValue,
+        depth: depth,
+        hasher: hasher,
+        ms: ms,
+        root: currentHash,
+    }
+}
+
+func keyToPath(key []byte) int {
+    path := 0
+    for _, b := range(key) {
+        path = (path << 8) + int(b)
+    }
+
+    return path
+}
+
+func (smt *SparseMerkleTree) Get(key []byte) ([]byte, error) {
+    // TODO: don't hardcode length of keys
+    currentValue := make([]byte, len(smt.root))
+    copy(currentValue, smt.root)
+    path := keyToPath(key)
+
+    for i := uint(0); i < smt.depth; i++ {
+        value, err := smt.ms.Get(currentValue)
+        if err != nil {
+            return nil, err
+        }
+        if (path >> 255) & 1 == 1 {
+            copy(currentValue, value[32:])
+        } else {
+            copy(currentValue, value[:32])
+        }
+        path <<= 1
+    }
+
+    value, err := smt.ms.Get(currentValue)
+    if err != nil {
+        return nil, err
+    }
+
+    return value, nil
 }
--- smt_test.go
+++ smt_test.go
@@ -6,7 +6,9 @@ import(
 )
 
 func TestSparseMerkleTree(t *testing.T) {
+    h := sha256.New()
     sm := NewSimpleMap()
-    smt := NewSparseMerkleTree(sm, []byte{0}, 256, sha256.New())
-    t.Log(smt)
+    smt := NewSparseMerkleTree(sm, []byte{0}, 256, h)
+    h.Write([]byte("test"))
+    t.Log(smt.Get(h.Sum(nil)))
 }
