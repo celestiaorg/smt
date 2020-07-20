5f09ca0a56a35f8d4b4ec647c6d167c62f21eab1
Mustafa Al-Bassam <mus@musalbas.com>
2020-07-20 18:19:43 +0100

    partial test fix

--- smt.go
+++ smt.go
@@ -220,8 +220,8 @@ func (smt *SparseMerkleTree) updateWithSideNodes(path []byte, value []byte, side
 		sideNode := make([]byte, smt.th.pathSize())
 
 		if sideNodes[i] == nil {
-			if commonPrefixCount != smt.depth() && commonPrefixCount > i {
-				// If there are no sidenodes at this height, and but the number of bits that the paths of the two leaf nodes share in common is greater than this height, then we need to build up the tree to this height with placeholder values at siblings.
+			if commonPrefixCount != smt.depth() && commonPrefixCount >= i {
+				// If there are no sidenodes at this height, but the number of bits that the paths of the two leaf nodes share in common is greater than or equal to this height, then we need to build up the tree to this height with placeholder values at siblings.
 				copy(sideNode, smt.th.placeholder())
 			} else {
 				continue
