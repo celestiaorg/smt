45dab71d6c81d96dbd7b4b09ec3d802d0edf2288
Mustafa Al-Bassam <mus@musalbas.com>
2020-08-04 15:55:02 +0100

    Refactor proof tests, add base case tests

--- proofs_test.go
+++ proofs_test.go
@@ -5,9 +5,89 @@ import (
 	"math/rand"
 	"reflect"
 	"testing"
+	"hash"
+	"bytes"
 )
 
-func TestProofs(t *testing.T) {
+type testUpdater func(key []byte, value []byte) ([]byte, error)
+type testProver func(key []byte) (SparseMerkleProof, error)
+type testVerifier func(proof SparseMerkleProof, root []byte, key []byte, value []byte, hasher hash.Hash) bool
+
+// Test base case Merkle proof operations.
+func TestProofsBasic(t *testing.T) {
+	var sm *SimpleMap
+	var smt *SparseMerkleTree
+
+	sm = NewSimpleMap()
+	smt = NewSparseMerkleTree(sm, sha256.New())
+	testProofsBasic(t, smt.Update, smt.Prove, VerifyProof)
+	
+	sm = NewSimpleMap()
+	smt = NewSparseMerkleTree(sm, sha256.New())
+	testProofsBasic(t, smt.Update, smt.ProveCompact, VerifyCompactProof)
+}
+
+func testProofsBasic(t *testing.T, update testUpdater, prove testProver, verify testVerifier) {
+	var proof SparseMerkleProof
+	var result bool
+	var root []byte
+	var err error
+
+	// Generate and verify a proof on an empty key.
+	proof, err = prove([]byte("testKey3"))
+	if err != nil {
+		t.Error("error returned when trying to prove inclusion on empty key")
+	}
+	result = verify(proof, bytes.Repeat([]byte{0}, sha256.New().Size()), []byte("testKey3"), defaultValue, sha256.New())
+	if !result {
+		t.Error("valid proof on empty key failed to verify")
+	}
+
+	t.Log(proof)
+
+	// Add a key, generate and verify a Merkle proof.
+	root, _ = update([]byte("testKey"), []byte("testValue"))
+	proof, err = prove([]byte("testKey"))
+	if err != nil {
+		t.Error("error returned when trying to prove inclusion")
+	}
+	result = verify(proof, root, []byte("testKey"), []byte("testValue"), sha256.New())
+	if !result {
+		t.Error("valid proof failed to verify")
+	}
+
+	// Add a key, generate and verify both Merkle proofs.
+	root, _ = update([]byte("testKey2"), []byte("testValue"))
+	proof, err = prove([]byte("testKey"))
+	if err != nil {
+		t.Error("error returned when trying to prove inclusion")
+	}
+	result = verify(proof, root, []byte("testKey"), []byte("testValue"), sha256.New())
+	if !result {
+		t.Error("valid proof failed to verify")
+	}
+	proof, err = prove([]byte("testKey2"))
+	if err != nil {
+		t.Error("error returned when trying to prove inclusion")
+		t.Log(err)
+	}
+	result = verify(proof, root, []byte("testKey2"), []byte("testValue"), sha256.New())
+	if !result {
+		t.Error("valid proof failed to verify")
+	}
+
+	// Generate and verify a proof on an empty key.
+	proof, err = prove([]byte("testKey3"))
+	if err != nil {
+		t.Error("error returned when trying to prove inclusion on empty key")
+	}
+	result = verify(proof, root, []byte("testKey3"), defaultValue, sha256.New())
+	if !result {
+		t.Error("valid proof on empty key failed to verify")
+	}
+}
+
+func TestProofsOld(t *testing.T) {
 	sm := NewSimpleMap()
 	smt := NewSparseMerkleTree(sm, sha256.New())
 	var err error
@@ -90,7 +170,6 @@ func TestProofs(t *testing.T) {
 	proof, err = smt.Prove([]byte("testKey3"))
 	if err != nil {
 		t.Error("error returned when trying to prove inclusion on empty key")
-		t.Log(err)
 	}
 	result = VerifyProof(proof, smt.root, []byte("testKey3"), defaultValue, sha256.New())
 	if !result {
