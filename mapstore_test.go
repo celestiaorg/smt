358c96615781568c405367c073becf1403336356
Mustafa Al-Bassam <mus@musalbas.com>
2020-08-12 22:51:42 +0100

    use err in mapstore test

--- mapstore_test.go
+++ mapstore_test.go
@@ -14,23 +14,26 @@ func TestSimpleMap(t *testing.T) {
 
 	h.Write([]byte("test"))
 
-	// Tests for Get
+	// Tests for Get.
 	_, err = sm.Get(h.Sum(nil))
 	if err == nil {
 		t.Error("did not return an error when getting a non-existent key")
 	}
 
-	// Tests for Put
+	// Tests for Put.
 	err = sm.Put(h.Sum(nil), []byte("hello"))
 	if err != nil {
 		t.Error("updating a key returned an error")
 	}
 	value, err = sm.Get(h.Sum(nil))
+	if err != nil {
+		t.Error("getting a key returned an error")
+	}
 	if bytes.Compare(value, []byte("hello")) != 0 {
 		t.Error("failed to update key")
 	}
 
-	// Tests for Del
+	// Tests for Del.
 	err = sm.Del(h.Sum(nil))
 	if err != nil {
 		t.Error("deleting a key returned an error")
