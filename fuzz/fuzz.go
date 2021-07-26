2e3797d09215e40d1a4ab3297a3491535479a99c
Cuong Manh Le <cuong@orijtech.com>
2021-07-27 00:40:41 +0700

    Add fuzz package for running go-fuzz (#51)
    While at it, also add oss fuzz build script for to prepare for adding to
    oss-fuzz later.
    
    Fixes #31

---
+++ fuzz/fuzz.go
@@ -0,0 +1,70 @@
+package fuzz
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"math"
+
+	"github.com/celestiaorg/smt"
+)
+
+func Fuzz(input []byte) int {
+	if len(input) < 100 {
+		return 0
+	}
+	smn, smv := smt.NewSimpleMap(), smt.NewSimpleMap()
+	tree := smt.NewSparseMerkleTree(smn, smv, sha256.New())
+	r := bytes.NewReader(input)
+	var keys [][]byte
+	key := func() []byte {
+		if readByte(r) < math.MaxUint8/2 {
+			k := make([]byte, readByte(r)/2)
+			r.Read(k)
+			keys = append(keys, k)
+			return k
+		}
+		return keys[int(readByte(r))%len(keys)]
+	}
+	for i := 0; r.Len() != 0; i++ {
+		b, err := r.ReadByte()
+		if err != nil {
+			continue
+		}
+		op := op(int(b) % int(Noop))
+		switch op {
+		case Get:
+			tree.Get(key())
+		case Update:
+			value := make([]byte, 32)
+			binary.BigEndian.PutUint64(value, uint64(i))
+			tree.Update(key(), value)
+		case Delete:
+			tree.Delete(key())
+		case Prove:
+			tree.Prove(key())
+		case Has:
+			tree.Has(key())
+		}
+	}
+	return 1
+}
+
+type op int
+
+const (
+	Get op = iota
+	Update
+	Delete
+	Prove
+	Has
+	Noop
+)
+
+func readByte(r *bytes.Reader) byte {
+	b, err := r.ReadByte()
+	if err != nil {
+		return 0
+	}
+	return b
+}
